b34745e3e50e2fbad880ba5452b048643306003b
Sven A. Schmidt <sas@finestructure.co>
2022-01-12 11:54:49 +0100

    Switch to @main / main() async / AsyncParsableCommand

--- Sources/ImporterCore/Importer.swift
+++ Sources/ImporterCore/Importer.swift
@@ -2,45 +2,31 @@ import ArgumentParser
 import Foundation
 
 
-//@main
-public struct Importer: ParsableCommand {
-
-    public init() {}
-
-    public mutating func run() throws {
-
-        Task {
-            do {
-                print("importing...")
-                let token = try Keychain.readString(
-                    service: "Github finestructure Personal Access Token",
-                    account: "finestructure"
-                )
-                let query = "in:path Package.swift"
-                let results = try await Github.search(query: query,
-                                                      token: token,
-                                                      page: 1, perPage: 100)
-                print("received \(results.items.count) / \(results.totalCount)")
-                let data = try JSONEncoder().encode(results.items)
-                let saveUrl = URL(fileURLWithPath: "/Users/sas/Downloads/results.json")
-                print("saving...")
-                try data.write(to: saveUrl)
-                _exit(0)
-            } catch {
-                _exit(error: error)
-            }
-        }
-
-        RunLoop.main.run()
+@main
+struct App {
+    static func main() async {
+        await Importer.main()
     }
-
 }
 
 
-func _exit(error: Error) -> Never {
-    let msg = (error as? Github.FetchError)?.localizedDescription ?? error.localizedDescription
-    print(msg)
-    fflush(stdout)
-    fflush(stderr)
-    _exit(1)
+struct Importer: AsyncParsableCommand {
+
+    func runAsync() async throws {
+        print("importing...")
+        let token = try Keychain.readString(
+            service: "Github finestructure Personal Access Token",
+            account: "finestructure"
+        )
+        let query = "in:path Package.swift"
+        let results = try await Github.search(query: query,
+                                              token: token,
+                                              page: 1, perPage: 100)
+        print("received \(results.items.count) / \(results.totalCount)")
+        let data = try JSONEncoder().encode(results.items)
+        let saveUrl = URL(fileURLWithPath: "/Users/sas/Downloads/results.json")
+        print("saving...")
+        try data.write(to: saveUrl)
+    }
+
 }
--- Sources/importer/importer.swift
+++ Sources/importer/importer.swift
@@ -1,3 +1,1 @@
 import ImporterCore
-
-Importer.main()
