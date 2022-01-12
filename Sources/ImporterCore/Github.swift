491133c755a7d58d500cea7529649ba624fff603
Sven A. Schmidt <sas@finestructure.co>
2022-01-12 15:50:08 +0100

    Add sortBy, oderBy

--- Sources/ImporterCore/Github.swift
+++ Sources/ImporterCore/Github.swift
@@ -22,9 +22,33 @@ enum Github {
         }
     }
 
+    enum OrderBy: String {
+        case ascending = "asc"
+        case descending = "desc"
+
+        var queryItem: URLQueryItem? { .init(name: "order", value: rawValue) }
+    }
+
+    enum SortBy: String {
+        case bestMatch
+        case forks
+        case stars
+        case updated
+
+        var queryItem: URLQueryItem? {
+            switch self {
+                case .bestMatch:
+                    return nil
+                default:
+                    return .init(name: "sort", value: rawValue)
+            }
+        }
+    }
+
     enum FetchError: Error {
         case invalidResponse
         case invalidURL
+        case noMoreResults
         case failed(String)
 
         var localizedDescription: String {
@@ -33,18 +57,24 @@ enum Github {
                     return "invalid response"
                 case .invalidURL:
                     return "invalid URL"
+                case .noMoreResults:
+                    return "no more results"
                 case .failed(let string):
                     return "error: \(string)"
             }
         }
     }
 
-    static func search(query: String, token: String, page: Int, perPage: Int = 100) async throws -> SearchResults {
+    // Github repository search API
+    // https://docs.github.com/en/rest/reference/search#search-repositories
+    static func search(query: String, sortBy: SortBy = .bestMatch,  orderBy: OrderBy = .ascending, token: String, page: Int, perPage: Int = 100) async throws -> SearchResults {
         var components = URLComponents(string: "https://api.github.com/search/repositories")!
         components.queryItems = ["q": query,
                                  "page": "\(page)",
                                  "per_page": "\(perPage)"]
             .map { URLQueryItem.init(name: $0, value: $1) }
+        + [sortBy.queryItem].compactMap { $0 }
+        + [orderBy.queryItem].compactMap { $0 }
         guard let url = components.url else {
             throw FetchError.invalidURL
         }
@@ -54,8 +84,13 @@ enum Github {
         guard let resp = resp as? HTTPURLResponse else {
             throw FetchError.invalidResponse
         }
-        guard resp.statusCode == 200 else {
-            throw FetchError.failed("unexpected status code: \(resp.statusCode)")
+        switch resp.statusCode {
+            case 200:
+                break
+            case 422:
+                throw FetchError.noMoreResults
+            default:
+                throw FetchError.failed("unexpected status code: \(resp.statusCode)")
         }
         return try JSONDecoder().decode(SearchResults.self, from: data)
     }
--- Sources/ImporterCore/Importer.swift
+++ Sources/ImporterCore/Importer.swift
@@ -35,27 +35,68 @@ struct Importer: AsyncParsableCommand {
     @Option(name: .shortAndLong)
     var output: Output = .file(URL(fileURLWithPath: "result.json"))
 
+    @Option(name: .long)
+    var order: Github.OrderBy = .descending
+
+    @Option(name: .long)
+    var sort: Github.SortBy = .updated
+
+    @Option
+    var page: Int?
+
     func runAsync() async throws {
         print("importing...")
         guard let token = Current.githubToken() else {
             throw AppError.missingGithubToken
         }
-        let results = try await Github.search(query: "in:path Package.swift",
-                                              token: token,
-                                              page: 1,
-                                              perPage: 100)
-        let items = results.items.map(\.cloneUrl)
-        print("received \(items.count) / \(results.totalCount)")
+
+        var packageURLs = [String]()
+
+        if let page = page {
+            let results = try await Github.search(query: "in:path Package.swift",
+                                                  token: token,
+                                                  page: page,
+                                                  perPage: 100)
+            packageURLs = results.items.map(\.cloneUrl)
+            print("received \(packageURLs.count) / \(results.totalCount)")
+        } else {  // fetch all
+            var currentPage = 1
+            while true {
+                defer { currentPage += 1 }
+                print("fetching page \(currentPage)")
+                do {
+                    let results = try await Github.search(query: "in:path Package.swift",
+                                                          sortBy: .updated,
+                                                          orderBy: .descending,
+                                                          token: token,
+                                                          page: currentPage,
+                                                          perPage: 100)
+                    let items = results.items.map(\.cloneUrl)
+                    packageURLs.append(contentsOf: items)
+                    print("fetched \(packageURLs.count) / \(results.totalCount)")
+                    if packageURLs.count >= results.totalCount {
+                        break
+                    }
+                } catch Github.FetchError.noMoreResults {
+                    break
+                }
+            }
+        }
+
         switch output {
             case .file(let url):
-                let data = try JSONEncoder().encode(items)
-                print("saving to \(url.path)...")
+                let data = try JSONEncoder().encode(packageURLs)
+            print("saving to \(url.path)...")
                 try data.write(to: url)
             case .stdout:
-                for item in items {
+                for item in packageURLs {
                     print(item)
                 }
         }
     }
 
 }
+
+
+extension Github.OrderBy: ExpressibleByArgument { }
+extension Github.SortBy: ExpressibleByArgument { }
