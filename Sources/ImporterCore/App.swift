923e27fab98982199de7504dc36aac3a91b7ac03
Sven A. Schmidt <sas@finestructure.co>
2022-01-16 05:44:32 +0100

    Tidy up

--- Sources/ImporterCore/App.swift
+++ Sources/ImporterCore/App.swift
@@ -2,13 +2,6 @@ import ArgumentParser
 
 
 @main
-struct Main {
-    static func main() async {
-        await App.main()
-    }
-}
-
-
 public struct App: ParsableCommand {
     public static var configuration = CommandConfiguration(
         abstract: "SPI Package Importer",
